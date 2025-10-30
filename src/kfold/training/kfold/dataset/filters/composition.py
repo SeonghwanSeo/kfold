4e5d000ef8b9f33e78861985f3bea16ac3506c64
SeonghwanSeo <shwan0106@gmail.com>
2025-10-30 21:31:07 +0900

    add filters from boltz

---
+++ src/kfold/training/kfold/dataset/filters/composition.py
@@ -0,0 +1,57 @@
+import kfold.constants as C
+from kfold.data.metadata import Metadata
+from kfold.utils.modality_utils.ligand import LIGAND_EXCLUSIONS
+from kfold.utils.registry import DATA_FILTER
+
+from .filter import DataFilter
+
+
+@DATA_FILTER.register()
+class CompositionFilter(DataFilter):
+    """Filter the data based on the composition of biomolecular complex
+
+    Rule:
+    - Filter out the single-chain data.
+    - Filter out the data without at least one protein chain.
+
+    TODO:
+    - (SeonghwanSeo) We may want to add ligand ccd filtering in teh future.
+      Currently we simply use boltz's preprocessed data, which already filters
+      out some common molecules.
+    """
+
+    class Config:
+        """
+        remove_excluding_ligands (bool): Whether to remove ligands with excluded CCDs.
+        """
+
+        remove_excluding_ligands: bool = False
+
+    def __init__(self, config: Config):
+        self.remove_excluding_ligands: bool = config.remove_excluding_ligands
+
+    def filter(self, metadata: Metadata) -> bool:
+        chains = metadata.chains
+
+        if self.remove_excluding_ligands:
+            # Remove ligands with excluded CCDs
+            chains = [
+                chain
+                for chain in metadata.chains
+                if not (
+                    chain.chain_type == C.chain.ChainType.Ligand
+                    and chain.ccd in LIGAND_EXCLUSIONS
+                )
+            ]
+
+        if len(chains) < 2:
+            # Remove single-chain data
+            return False
+
+        has_protein = any(
+            chain.chain_type == C.chain.ChainType.Protein for chain in chains
+        )
+        if not has_protein:
+            return False
+
+        return True
