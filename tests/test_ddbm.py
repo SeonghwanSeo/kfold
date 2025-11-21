f61c8dc5dd69ddb2de5f6d2fc9adfd8c622f33e2
SeonghwanSeo <shwan0106@gmail.com>
2025-11-21 14:13:30 +0900

    fix

--- tests/test_ddbm.py
+++ tests/test_ddbm.py
@@ -7,6 +7,7 @@ import kfold.model.modules as submodules
 from kfold.config import load_config
 from kfold.data.model_input import FoldingInput
 from kfold.data.structure import TokenizedStructure
+from kfold.model.modules.structure_module.kfold_ddbm import KFoldBridgeDiffusion
 from kfold.training.folding.dataset.datamodule import TrainingDataModule
 from kfold.utils import errors
 from kfold.utils.registry import Registry
@@ -37,8 +38,8 @@ if __name__ == "__main__":
     score_model: submodules.score_model.BaseScoreModel = Registry.instantiate(
         model_config.score_model
     )
-    structure_module: submodules.structure_module.BaseStructureModule = (
-        Registry.instantiate(model_config.structure_module, score_model=score_model)
+    structure_module: KFoldBridgeDiffusion = Registry.instantiate(
+        model_config.structure_module, score_model=score_model
     )
     score_model = score_model.to(device="cuda")
     score_model.eval()
@@ -54,9 +55,10 @@ if __name__ == "__main__":
     for iter, (f_input, full_dict_list) in enumerate(dataloader):
         assert f_input.batch_size == 1
 
-        if iter < 10:
+        if iter == 5:
             break
 
+        # ====== Save original structures ====== #
         full_dict = full_dict_list[0]
         name: str = full_dict["id"]
         struct = full_dict["structure"]
@@ -77,7 +79,6 @@ if __name__ == "__main__":
             print(f"Skipping {name} due to PDB writing error.")
 
         # Save structure with data augmentation during featurization
-        # Replace coords
         struct = struct.replace_atom_coords(
             atom_coords=f_input.atom.label_coords[0].cpu().numpy().transpose(1, 0, 2),
         )
@@ -94,19 +95,32 @@ if __name__ == "__main__":
             save_apo=True,
         )
 
+        # ====== DDBM Sampling ====== #
         # Move to GPU
         f_input = f_input.to(device="cuda")
 
         # Get sigmas
-        t_hat = structure_module.sample_noise_level(1, num_samples, f_input.device)
-        t_hat = torch.sort(t_hat, dim=-1).values  # [1, num_samples]
-        t_hat = t_hat / 5
+        if True:
+            t_hat = structure_module.sample_noise_level(1, num_samples, f_input.device)
+            # Override to min and max sigmas
+            t_hat[0, 0] = structure_module.sigma_min
+            t_hat[0, 1] = structure_module.sigma_max
+            t_hat = torch.sort(t_hat, dim=-1).values  # [1, num_samples]
+        else:
+            t_hat = torch.linspace(
+                structure_module.sigma_min,
+                structure_module.sigma_max,
+                num_samples,
+                device=f_input.device,
+            ).unsqueeze(0)
+        print(t_hat)
 
         # Get label coords # [1, num_samples, Natom, 3]
         label_coords = structure_module.sample_holo(f_input, num_samples)
 
         # Get apo coords # [1, num_samples, Natom, 3]
         apo_coords = structure_module.sample_prior(f_input, num_samples, label_coords)
+        # apo_coords = apo_coords + 100  # shift
 
         noisy_coords = structure_module.interpolate(
             apo_coords,
@@ -124,17 +138,27 @@ if __name__ == "__main__":
             is_apo=True,
         )
 
-        for i in range(num_samples):
+        if structure_module.coordinate_augmentation:
+            for i in range(num_samples):
+                # Save struct
+                struct.to_pdb(
+                    SAVE_PATH / f"{name}-gt-{i}.pdb",
+                    is_predicted=False,
+                )
+                struct.to_pdb(
+                    SAVE_PATH / f"{name}-apo-{i}.pdb",
+                    save_apo=True,
+                )
+        else:
             # Save struct
             struct.to_pdb(
-                SAVE_PATH / f"{name}-gt-{i}.pdb",
+                SAVE_PATH / f"{name}-ddbm-holo.pdb",
                 is_predicted=False,
             )
             struct.to_pdb(
-                SAVE_PATH / f"{name}-apo-{i}.pdb",
+                SAVE_PATH / f"{name}-ddbm-apo.pdb",
                 save_apo=True,
             )
-            break
 
         struct = struct.replace_atom_coords(
             atom_coords=noisy_coords[0].cpu().numpy(),
@@ -142,8 +166,7 @@ if __name__ == "__main__":
         for i in range(num_samples):
             # Save struct
             struct.to_pdb(
-                SAVE_PATH / f"{name}-int-{i}.pdb",
+                SAVE_PATH / f"{name}-ddbm-traj-{i}.pdb",
                 conformer_id=i,
                 is_predicted=False,
             )
-        break
