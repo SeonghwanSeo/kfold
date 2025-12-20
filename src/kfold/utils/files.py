3da54d1cdcce6ed1514b312cdbb7006b0061a305
SeonghwanSeo <shwan0106@gmail.com>
2025-12-20 21:59:00 +0900

    support apo structure cif format

--- src/kfold/utils/files.py
+++ src/kfold/utils/files.py
@@ -2,38 +2,51 @@ from pathlib import Path
 
 import gemmi
 
+Point3D = tuple[float, float, float]
+
 
 def load_apo_chain(
-    pdb_path: str | Path,
-) -> dict[int, tuple[str, dict[str, tuple[float, float, float]]]]:
+    path: str | Path,
+) -> tuple[dict[int, str], dict[int, dict[str, Point3D]]]:
     """Load a apo polymer chain from a PDB file using gemmi.
 
     Parameters
     ----------
-    pdb_path : Path
-        Path to the predicted PDB file (e.g., AFDB, ESMFold, ...)
+    path : Path
+        Path to the predicted structure file (e.g., AFDB, ESMFold, ...)
 
     Returns
     -------
-    dict[int, tuple[str, dict[str, tuple[float, float, float]]]]
+    sequences : dict[int, str]
+        key: residue index (1-based)
+        value: residue name (three-letter code)
+    atom_coordinates: dict[int, dict[str, tuple[float, float, float]]]
         key: residue index (1-based)
         value:
-            - residue name (str)
             - dict of atom name to coordinates (x, y, z) as np.ndarray
 
     """
 
-    structure: gemmi.Structure = gemmi.read_pdb(str(pdb_path))
+    filetype = Path(path).suffix.lower()
+    assert filetype in {".pdb", ".cif"}, f"Unsupported file type: {filetype}"
+
+    structure: gemmi.Structure
+    if filetype == ".cif":
+        structure = gemmi.read_structure(str(path))
+    else:
+        structure = gemmi.read_pdb(str(path))
     raw_chain = structure[0].subchains()[0]
 
-    results: dict[int, tuple[str, dict[str, tuple[float, float, float]]]] = {}
+    sequences: dict[int, str] = {}
+    atom_coordinates: dict[int, dict[str, tuple[float, float, float]]] = {}
     for res in raw_chain:
         res: gemmi.Residue
         res_name = res.name
         res_idx = int(res.seqid.num)
-        name_to_atom_coords = {
+        atom_name_to_coords = {
             atom.name: (atom.pos.x, atom.pos.y, atom.pos.z) for atom in res
         }
-        results[res_idx] = (res_name, name_to_atom_coords)
+        sequences[res_idx] = res_name
+        atom_coordinates[res_idx] = atom_name_to_coords
 
-    return results
+    return sequences, atom_coordinates
