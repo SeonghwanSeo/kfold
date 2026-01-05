f1cb33fe50afa307c9046c8d8300a4ec71dcf5b9
SeonghwanSeo <shwan0106@gmail.com>
2026-01-06 01:18:02 +0900

    move tests

---
+++ tests/apo_perturbation/test_simple_apo_perturbation.py
@@ -0,0 +1,63 @@
+import random
+from pathlib import Path
+
+from kfold.data.pipelines._apo_perturbation import (
+    ApoPerturbation,
+    ApoPerturbationConfig,
+    LangevinConfig,
+)
+from kfold.data.utils.io.structure import read_protein_structure, write_protein_structure
+from kfold.data.utils.simulation.rieprody import RieProdyConfig
+
+if __name__ == "__main__":
+    data_dir = Path("/cache/wykim_lab/kfold_data/v260103/dataset/rcsb-train/")
+    save_dir = Path("./tmp")
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    # create rieprody config
+    rieprody_config = RieProdyConfig(
+        metric_lmdb_path=data_dir / "rieprody_metric.lmdb",
+        rmsd_threshold=100.0,  # disable rmsd filtering for testing
+    )
+    langevin_config = LangevinConfig(
+        min_steps=1,
+        max_steps=5,
+        dt=0.09,  # N(0, 0.04) noise
+        res_r=4.0,
+        bond_r=4.0,
+        ent_r=10.0,
+        sphere_r=10.0,
+    )
+
+    module = ApoPerturbation(
+        ApoPerturbationConfig(
+            rieprody=rieprody_config,
+            langevin=langevin_config,
+        )
+    )
+
+    # Example usage
+    files = sorted(list((data_dir / "apo").rglob("*.pdb.gz")))
+    random.seed(42)
+    random.shuffle(files)
+
+    for pdb_file in files[:10]:  # test on one file
+        print(f"Processing {pdb_file}")
+        name = pdb_file.name.split(".")[0]
+        seq, apo_coords = read_protein_structure(pdb_file)
+
+        # Save original structure
+        output_file = save_dir / f"{name}.pdb"
+        write_protein_structure(seq, apo_coords, output_file)
+
+        # # Rieprody perturbation
+        # module.prob_rieprody = 1.0  # always use rieprody for testing
+        # perturb_coords = module.run(apo_coords, key=name)
+        # output_file = save_dir / f"{name}_rieprody.pdb"
+        # write_protein_structure(seq, perturb_coords, output_file)
+
+        # Langevin perturbation
+        module.prob_rieprody = 0.0  # always use langevin for testing
+        perturb_coords = module.run(apo_coords)
+        output_file = save_dir / f"{name}_langevin.pdb"
+        write_protein_structure(seq, perturb_coords, output_file)
