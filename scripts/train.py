2dc10625f2ac173e1316d7e1a6a4a833107bfb8f
SeonghwanSeo <shwan0106@gmail.com>
2026-01-29 23:05:42 +0900

    minor update: version; benchmark

--- scripts/train.py
+++ scripts/train.py
@@ -264,6 +264,7 @@ def build_trainer(cfg, debug: bool = False, skip_val: bool = False) -> pl.Traine
         accumulate_grad_batches=pl_trainer_cfg.accumulate_grad_batches,
         gradient_clip_val=pl_trainer_cfg.gradient_clip_val,
         use_distributed_sampler=False,
+        benchmark=True,
         # reload_dataloaders_every_n_epochs=1,
     )
     return trainer
