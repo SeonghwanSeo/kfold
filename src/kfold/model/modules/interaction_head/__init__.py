ce5fb7ed34a04b97cd70b8ab190dd175f2a75721
SeonghwanSeo <shwan0106@gmail.com>
2026-03-18 13:10:08 +0900

    remove unused files
    remove remaining unused

--- src/kfold/model/modules/interaction_head/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import BaseInteractionHead  # noqa: F401
-from .linear_head import InteractionHead  # noqa: F401
--- src/kfold/training/loss/__init__.py
+++ src/kfold/training/loss/__init__.py
@@ -1,1 +1,1 @@
-from . import confidence, diffusion, distogram, interaction
+from . import confidence, diffusion, distogram
